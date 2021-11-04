d26e8cc4584269588c55a5a0f5a943231eda9e7a
leo95h <leonardo.lhpr@gmail.com>
2021-11-04 10:57:23 -0300

    Starting a new version: 2.0.0

---
+++ backend/src/main/java/br/com/gori/scb/ScbApplication.java
@@ -0,0 +1,13 @@
+package br.com.gori.scb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScbApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/gori/scb/ScbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gori.scb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
